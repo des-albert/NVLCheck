4d6ca0c7ea689f4381b8843bec456881e1a9aa2b
Des Albert <des.albert@gmail.com>
2026-02-06 09:44:40 -0800

    version update

--- src/main/kotlin/org/dba/nvlcheck/NVLCheck.kt
+++ src/main/kotlin/org/dba/nvlcheck/NVLCheck.kt
@@ -98,10 +98,10 @@ class NVLCheck {
     @FXML
     lateinit var labelJDK: Label
 
-    private val NVL72_GB300 = "NVL72 GB300 Configurator v13.0.xlsx"
+    private val NVL72_GB300 = "NVL72 GB300 Configurator v14.0.xlsx"
     private val NVL72_GB200 = "NVL72 GB200 Configurator v5.0.xlsx"
-    private val NVL4_GB200 = "NVL4 GB200 Configurator v6.0.xlsx"
-    private val NVL72_VR200 = "NVL72 VR200 Configurator v1.0.xlsx"
+    private val NVL4_GB200 = "NVL4 GB200 Configurator v7.0.xlsx"
+    private val NVL72_VR200 = "NVL72 VR200 Configurator v2.0.xlsx"
 
     private val configRootPath = "C:\\Users\\albertd\\OneDrive - Hewlett Packard Enterprise\\HPE\\NVL\\"
     private var sourcePath = configRootPath + NVL72_GB300
